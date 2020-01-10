6d4b4e9c820fdacd4d47430af2b89e8223267936
Spike <gflamethrower@gmail.com>
2020-01-09 21:52:57 -0500

    changed from System.out.print to System.out.println

--- src/Application.java
+++ src/Application.java
@@ -1,8 +1,7 @@
 
 public class Application {
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
-		System.out.print("Testing updates to git!");
+		System.out.println("Hello World!");
+		System.out.println("Testing updates to git!");
 	}
-
 }
